e4b928fc2721f1d580effe594904df9c61fb1b86
kishore-fyers <kishore.kumar@fyers.in>
2024-08-03 16:41:10 +0530

    UPDATE: user validation

--- src/components/RequireAuth.tsx
+++ src/components/RequireAuth.tsx
@@ -1,8 +1,26 @@
+import { useEffect, useState } from "react";
 import { Navigate } from "react-router-dom";
 import { useSigninCheck } from "reactfire";
+import { IdTokenResult } from "firebase/auth";
 
 export function RequireAuth({ children }) {
+  const [role, setRole] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
   const { status, data: signInCheckResult } = useSigninCheck();
+  
+  useEffect(() => {
+    if (status === "success") {
+      signInCheckResult.user
+        ?.getIdTokenResult(true)
+        .then((token: IdTokenResult) => {
+          console.log("token", token);
+          console.log("hereeee")
+          console.log(token.claims)
+          setRole(token.claims.role as string);
+          setIsLoading(false);
+        });
+    }
+  }, [status]);
 
   if (status === "loading") {
     return <span>loading...</span>;
